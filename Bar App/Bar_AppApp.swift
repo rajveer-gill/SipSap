5bae24e858cce304d1845c03800cb42446009573
Rajveer Gill <rajsgill03@gmail.com>
2024-05-08 21:15:09 -0700

    Initial Commit

---
+++ Bar App/Bar_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bar_AppApp.swift
+//  Bar App
+//
+//  Created by Raj on 5/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Bar_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
